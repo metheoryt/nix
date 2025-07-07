233e2bd675c7e09a2cfa3d0ce47cc124a9425137
Maxim Romanyuk <metheoryt@gmail.com>
2025-07-08 03:49:59 +0500

    fix

--- hosts/g16/me.nix
+++ hosts/g16/me.nix
@@ -19,6 +19,14 @@
   programs.firefox.enable = true;
   programs.fish.enable = true;
 
+  # Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # set custom UI scaling
   dconf = {
     enable = true;
