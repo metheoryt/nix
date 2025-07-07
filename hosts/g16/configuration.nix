c98dffb6289e06ce8364362817a12616f675075a
Maxim Romanyuk <metheoryt@gmail.com>
2025-07-08 03:51:34 +0500

    fix

--- hosts/g16/configuration.nix
+++ hosts/g16/configuration.nix
@@ -108,6 +108,14 @@
     wget
   ];
 
+  # Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
