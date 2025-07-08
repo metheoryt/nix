e262ff6a4f5b5c432c6dc26341808f8fa5094655
Maxim Romanyuk <metheoryt@gmail.com>
2025-07-08 17:17:52 +0500

    add grub

--- hosts/g16/configuration.nix
+++ hosts/g16/configuration.nix
@@ -12,6 +12,9 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
